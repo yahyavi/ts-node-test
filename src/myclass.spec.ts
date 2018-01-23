3bcf0cba6e1c6b65061662b9e8a7fbd7af48c97d
Saman Amraii <saman@xseer.io>
2018-01-23 13:12:55 -0800

    with modernizr

--- src/myclass.spec.ts
+++ src/myclass.spec.ts
@@ -2,7 +2,12 @@ import "mocha";
 import {expect} from "chai";
 import {MyClass} from "./myclass";
 
-it("test", () => {
+it("normal test", () => {
     const instance = new MyClass();
     expect(instance.sum(1, 2)).to.equal(3);
 });
+
+it("modernizer test", () => {
+    const instance = new MyClass();
+    expect(instance.modern()).to.equal(false);
+})
--- src/myclass.ts
+++ src/myclass.ts
@@ -1,5 +1,14 @@
+import * as Modernizr from "modernizr";
+
 export class MyClass {
     public sum(a: number, b: number) {
         return a + b;
+    }
+
+    public modern(): boolean {
+        if (Modernizr.webgl) {
+            return true;
+        }
+        return false;
     }
 }
