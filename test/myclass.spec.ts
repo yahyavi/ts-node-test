f67029ad8fb12d1b7cdbae8c1a4410646e8a780f
Saman Amraii <saman@xseer.io>
2018-01-25 14:14:11 -0800

    added sinon sandbox

--- test/myclass.spec.ts
+++ test/myclass.spec.ts
@@ -1,9 +1,18 @@
 import {expect} from "./expect";
 import * as Modernizr from "modernizr";
 import {MyClass} from "../src/myclass";
+import * as sinon from "sinon";
 require("jsdom-global")(); // this should come after Modernizr import!!!!
 
 describe("a series of sample tests", () => {
+    beforeEach(() => {
+        this.sinon = sinon.sandbox.create();
+    });
+
+    afterEach(() => {
+        this.sinon.restore();
+    })
+
     it("normal test", () => {
         const instance = new MyClass();
         expect(instance.sum(1, 2)).to.equal(3);
