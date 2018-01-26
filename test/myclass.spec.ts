482d76ff9977891f599cb2d815e30fe390e2e3c1
Saman Amraii <saman@xseer.io>
2018-01-25 18:10:16 -0800

    the result of await should be returned!

--- test/myclass.spec.ts
+++ test/myclass.spec.ts
@@ -21,10 +21,10 @@ describe("a series of sample tests", () => {
         });
     });
 
-    it("await test", async () => {
+    it("await test", () => {
         const instance = new MyClass();
         // const result = await instance.awaitSum(5, 2); // result will be 7
-        return expect(instance.awaitSum(5, 2)).to.eventually.equal(70);
+        return expect(instance.awaitSum(5, 2)).to.eventually.equal(7);
     });
 
     it("normal test", () => {
